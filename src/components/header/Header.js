6c518434f30007ea24f96fbe32202efee8f3e527
JoileJr <joilejunior2@gmail.com>
2023-06-28 15:14:44 -0300

    ajustes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,16 +1,37 @@
-import React from 'react'
-import styles from './Header.module.css'
-import SearchInput from '../searchInput/SearchInput';
+import { styled } from "styled-components"
+import SearchInput from "../searchInput/SearchInput"
 
-const Header = () => {
-  return (
-    <div className={styles.tagHeader}>
-        <a className={styles.logo} href="/">Boutique da Daiani</a>
-        <div>
-          <SearchInput/>
-        </div>
+const TagHeader = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 20px 160px;
+
+    > div {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 24px;
+    }
+`
+
+const Logo = styled.a`
+    color: var(--logo-color);
+    font-weight: 400;
+    font-size: 40px;
+    line-height: 150%;
+    font-family: 'Saira Stencil One', cursive;
+`
+
+const Header = (props) => {
+  return(
+    <TagHeader>
+    <Logo>Boutique da Daiani</Logo>
+    <div>
+        <SearchInput/>
     </div>
-  )
+    </TagHeader>
+    )
 }
 
 export default Header
